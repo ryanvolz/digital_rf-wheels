ff50df8a1365d53632fa17b034d110c5aa205e74
Ryan Volz <rvolz@mit.edu>
2018-02-05 17:12:26 -0500

    Install deps at beginning of pre_build, fix numpy spec.

--- config.sh
+++ config.sh
@@ -6,6 +6,9 @@ function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
     local wheelhouse=$(abspath ${WHEEL_SDIR:-wheelhouse})
+    if [ -n "$BUILD_DEPENDS" ]; then
+        pip install $(pip_opts) $BUILD_DEPENDS
+    fi
     mkdir digital_rf/python/build
     cd digital_rf/python/build
     cmake ..
