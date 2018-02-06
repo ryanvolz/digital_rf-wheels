c405c6fecb513810150effd382c9c5213bb2a856
Ryan Volz <rvolz@mit.edu>
2018-02-06 16:12:29 -0500

    Fix fetch_unpack_index.

--- prepare.sh
+++ prepare.sh
@@ -4,10 +4,14 @@ function fetch_unpack_index {
     local project_spec=$1
     [ -z "$project_spec" ] && echo "project_spec not defined" && exit 1
     local source_dir=${2:-src}
-    rm_mkdir arch_tmp
-    pip download $(pip_opts) --no-deps --no-binary :all: -d arch_tmp $project_spec
-    local out_archive=$(ls arch_tmp/*)
-    rm_mkdir $source_dir
-    (cd $source_dir && untar ../$out_archive && rsync --delete -avh * ..)
+    rm_mkdir dl_tmp
+    pip download $(pip_opts) --no-deps --no-binary :all: -d dl_tmp $project_spec
+    local out_archive=$(ls dl_tmp/*)
+    mkdir -p $source_dir
+    (cd $source_dir \
+        && rm_mkdir arch_tmp \
+        && cd arch_tmp \
+        && untar ../$out_archive \
+        && rsync --delete -avh * ..)
 }
 
