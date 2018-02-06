82e76cfb5444700c7d46ef84ddef1500fa98f4a4
Ryan Volz <rvolz@mit.edu>
2018-02-06 15:40:05 -0500

    Fix by customizing fetching and unpacking from index.

--- config.sh
+++ config.sh
@@ -2,8 +2,18 @@
 # Test for OSX with [ -n "$IS_OSX" ]
 
 
+function fetch_unpack_index {
+    local project_spec=$1
+    [ -z "$project_spec" ] && echo "project_spec not defined" && exit 1
+    local source_dir=${2:-src}
+    rm_mkdir arch_tmp
+    pip download $(pip_opts) --no-deps --no-binary :all: -d arch_tmp $project_spec
+    local out_archive=$(ls arch_tmp/*)
+    rm_mkdir $source_dir
+    (cd $source_dir && untar ../$out_archive && rsync --delete -avh * ..)
+}
+
 function build_wheel {
-    local repo_dir=${1:-$REPO_DIR}
     if [ -z "$IS_OSX" ]; then
         build_linux_wheel $@
     else
