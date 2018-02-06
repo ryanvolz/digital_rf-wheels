117a2754396b3375ce68cdd1797a113def937465
Ryan Volz <rvolz@mit.edu>
2018-02-06 11:37:03 -0500

    Fix relative paths in prepare_source by switching to absolute.

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 function prepare_source {
-    local repo_dir=${1:-$REPO_DIR}
-    local src_dir=${2:-src}
+    local repo_dir=$(abspath ${1:-$REPO_DIR})
+    local src_dir=$(abspath ${2:-src})
     if [ -z "$IS_OSX" ]; then
         sudo apt-get install cmake
     else
@@ -9,7 +9,7 @@ function prepare_source {
     pip install -r ${repo_dir}/python/dev_requirements.txt
     mkdir -p ${src_dir}
     pushd ${src_dir}
-    cmake $(relpath ${repo_dir}/python)
+    cmake ${repo_dir}/python
     popd
 }
 
