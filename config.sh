9adbf56c8dc15cab6c8c5333811b4e813db5b88b
Ryan Volz <rvolz@mit.edu>
2018-04-02 17:40:19 -0400

    travis: Upgrade numpy explicitly because install_wheel fails.

--- config.sh
+++ config.sh
@@ -70,8 +70,9 @@ function build_osx_wheel {
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
-    # do our own install_wheel to pass -U flag to upgrade packages (i.e. numpy)
-    install_wheel -U
+    # upgrade numpy to most recent version (if build version is still around)
+    pip install -U numpy
+    # test
     pytest ../src
 }
 
