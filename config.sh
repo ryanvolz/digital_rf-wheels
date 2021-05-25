5d6755179cc55580ff5c0dabeb9aeb558009dda6
Ryan Volz <rvolz@mit.edu>
2021-05-25 16:44:23 -0400

    Back to explicit download and then build_wheel.

--- config.sh
+++ config.sh
@@ -1,24 +1,6 @@
 # Define custom utilities
 # Test for OSX with [ -n "$IS_OSX" ]
 
-function fetch_unpack_index {
-    local package_name=$1
-    local project_spec=$2
-    [ -z "$project_spec" ] && echo "project_spec not defined" && exit 1
-    local source_dir=${3:-src}
-    rm_mkdir dl_tmp
-    pip download $(pip_opts) --no-deps --no-binary $package_name -d dl_tmp $project_spec
-    local out_archive=$(abspath $(ls dl_tmp/*))
-    mkdir -p $source_dir
-    (cd $source_dir \
-        && rm_mkdir arch_tmp \
-        && cd arch_tmp \
-        && untar $out_archive \
-        && rsync --remove-source-files -avh */ .. \
-        && cd .. \
-        && rm -rf arch_tmp)
-}
-
 function pip_opts {
     # Extra options for pip
     echo "--verbose"
@@ -34,9 +16,7 @@ function pre_build {
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
-    # download the source
-    fetch_unpack_index $PACKAGE_NAME $PROJECT_SPEC src
     # test
-    pytest src
+    pytest ../src
 }
 
