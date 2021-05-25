8609bae928283ba74421a4e4677275a12d4748b3
Ryan Volz <rvolz@mit.edu>
2021-05-25 18:31:52 -0400

    Fix fetching source.

--- config.sh
+++ config.sh
@@ -1,6 +1,26 @@
 # Define custom utilities
 # Test for OSX with [ -n "$IS_OSX" ]
 
+SRC_DIR=src
+
+function fetch_unpack_index {
+    local package_name=$1
+    local project_spec=$2
+    [ -z "$project_spec" ] && echo "project_spec not defined" && exit 1
+    local source_dir=${3:-$SRC_DIR}
+    rm_mkdir dl_tmp
+    pip download $(pip_opts) --no-deps --no-binary $package_name -d dl_tmp $project_spec
+    local out_archive=$(abspath $(ls dl_tmp/$package_name*))
+    mkdir -p $source_dir
+    (cd $source_dir \
+        && rm_mkdir arch_tmp \
+        && cd arch_tmp \
+        && untar $out_archive \
+        && rsync --remove-source-files -avh */ .. \
+        && cd .. \
+        && rm -rf arch_tmp)
+}
+
 function pip_opts {
     # Extra options for pip
     echo "--verbose"
@@ -17,6 +37,6 @@ function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
     # test
-    pytest ../src
+    pytest ../$SRC_DIR
 }
 
